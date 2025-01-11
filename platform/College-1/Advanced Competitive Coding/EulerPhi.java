c63e2d8bc102a2461c964a0de46113eae315b20c
sayande717 <sayande717@github.com>
2025-01-11 21:32:21 +0530

    .

---
+++ platform/College-1/Advanced Competitive Coding/EulerPhi.java
@@ -0,0 +1,11 @@
+class EulerPhi {
+    private static int euler(int a, int b){
+        int c = Math.min(a,b);
+        for(int iter=a-1;iter>0;iter--) {
+            if(a%iter==0) && (b%iter==0) {
+                hcf = iter;
+                break;
+            }
+        }
+    }
+}
